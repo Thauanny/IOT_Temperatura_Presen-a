213628e41caa9af8dc0222d6d3306dfe60e372f2
Gabriel Santana Oliveira de Paula <gabsop@hotmail.com>
2023-10-26 10:28:04 -0300

    Update temperatura_presenca.ino
    FINAL

--- temperatura_presenca.ino
+++ temperatura_presenca.ino
@@ -1,4 +1,21 @@
 #include "DHT.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <PubSubClient.h>
+
+WiFiClient wifi_client;
+PubSubClient mqtt_client(wifi_client);
+
+const char* wifi_ssid = "Gabriel’s iPhone";
+const char* wifi_password = "umasenha";
+int wifi_timeout = 100000;
+
+const char* mqtt_broker = "io.adafruit.com";
+const int mqtt_port = 1883;
+int mqtt_timeout = 10000;
+
+const char* mqtt_usernameAdafruitIO = "Gabsop";
+const char* mqtt_keyAdafruitIO = "aio_hgRK23qm6Mf5zNvXLsyGJ4CpMm62";
 
 #define DHTPIN 4
 #define DHTTYPE DHT11
@@ -14,10 +31,19 @@ int targetTemperature = 25;
 
 void setup() {
   Serial.begin(9600);
+  
   Serial.println(F("Begin!"));
   Serial.print(F("People: "));
   Serial.println(people);
 
+  // Configurando Wifi
+  WiFi.mode(WIFI_STA); //"station mode": permite o ESP32 ser um cliente da rede WiFi
+  WiFi.begin(wifi_ssid, wifi_password);
+  connectWiFi();
+  mqtt_client.setServer(mqtt_broker, mqtt_port);
+
+
+  // Configurando Pinos
   pinMode(ENTRY_PIN, INPUT);
   pinMode(EXIT_PIN, INPUT);
 
@@ -25,56 +51,101 @@ void setup() {
 }
 
 void loop() {
-  delay(2000);
+  delay(500);
 
-  Serial.println(digitalRead(ENTRY_PIN));
-
-  if (digitalRead(ENTRY_PIN) == LOW) {
-    people++;
-    Serial.print(F("People: "));
-    Serial.println(people);
-    if (people % MIN_PEOPLE_TO_ADJUST == 0) {
-      adjustTemperatureMessage(true); // Increase temperature
-    }
-  }
-  if (digitalRead(EXIT_PIN) == LOW) {
-    if (people > 0) {
-      people--;
-      Serial.print(F("People: "));
-      Serial.println(people);
-      if (people % MIN_PEOPLE_TO_ADJUST == 0) {
-        adjustTemperatureMessage(false); // Decrease temperature
-      }
-    }
+  if (!mqtt_client.connected()) { // Se MQTT não estiver conectado
+    connectMQTT();
   }
 
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
 
-  if (isnan(h) || isnan(t)) {
+  if (isnan(humidity) || isnan(temperature)) {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
-  float hic = dht.computeHeatIndex(t, h, false);
+  if (digitalRead(ENTRY_PIN) == HIGH && mqtt_client.connected()) {
+    people++;
+    Serial.print(F("People: "));
+    Serial.println(people);
+    mqtt_client.publish("Gabsop/feeds/tempwarden-pro-max-v2.people-in-room", String(people).c_str(), true);
 
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C Heat Index: "));
-  Serial.print(hic);
-  Serial.println(F("°C "));
+    Serial.println(F("Someone entered the room"));
+    if (people % MIN_PEOPLE_TO_ADJUST == 0) {
+      adjustTemperatureMessage(true, temperature, humidity); // Increase temperature
+    }
+  }
+
+  if (digitalRead(EXIT_PIN) == HIGH && mqtt_client.connected() && people > 0) {
+    people--;
+    Serial.print(F("People: "));
+    Serial.println(people);
+    mqtt_client.publish("Gabsop/feeds/tempwarden-pro-max-v2.people-in-room", String(people).c_str(), true);
+    Serial.println(F("Someone left the room"));
+    if (people % MIN_PEOPLE_TO_ADJUST == 0) {
+      adjustTemperatureMessage(false, temperature, humidity); // Decrease temperature
+    }   
+  }
 }
 
-void adjustTemperatureMessage(bool increase) {
-  if (increase) {
+void adjustTemperatureMessage(bool increase, float temperature, float humidity) {
+  if (!increase) {
     Serial.println("Increase temperature on the air conditioner.");
     targetTemperature += 1;
   } else {
     Serial.println("Decrease temperature on the air conditioner.");
     targetTemperature -= 1;
   }
-  Serial.print("Target Temperature: ");
-  Serial.println(targetTemperature);
+
+  if (mqtt_client.connected()){
+    mqtt_client.loop();
+    
+    Serial.print("Target Temperature: ");
+    Serial.println(targetTemperature);
+    
+    mqtt_client.publish("Gabsop/feeds/tempwarden-pro-max-v2.target-temperature", String(targetTemperature).c_str(), true);
+    mqtt_client.publish("Gabsop/feeds/tempwarden-pro-max-v2.should-increase-or-decrease", String(increase).c_str(), true);
+    mqtt_client.publish("Gabsop/feeds/tempwarden-pro-max-v2.temperature", String(temperature).c_str(), true);
+    mqtt_client.publish("Gabsop/feeds/tempwarden-pro-max-v2.humidity", String(humidity).c_str(), true);
+    delay(1000);
+  }
+}
+
+void connectWiFi() {
+  Serial.print("Conectando à rede WiFi");
+
+  unsigned long tempoInicial = millis();
+  while (WiFi.status() != WL_CONNECTED && (millis() - tempoInicial < wifi_timeout)) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Conexão com WiFi falhou!");
+  } else {
+    Serial.print("Conectado com o IP: ");
+    Serial.println(WiFi.localIP());
+  }
+}
+
+void connectMQTT() {
+  unsigned long tempoInicial = millis();
+  while (!mqtt_client.connected() && (millis() - tempoInicial < mqtt_timeout)) {
+    if (WiFi.status() != WL_CONNECTED) {
+      connectWiFi();
+    }
+    Serial.print("Conectando ao MQTT Broker..");
+
+    if (mqtt_client.connect("ESP32Client", mqtt_usernameAdafruitIO, mqtt_keyAdafruitIO)) {
+      Serial.println();
+      Serial.print("Conectado ao broker MQTT!");
+    } else {
+      Serial.println();
+      Serial.print("Conexão com o broker MQTT falhou!");
+      delay(500);
+    }
+  }
+  Serial.println();
 }
