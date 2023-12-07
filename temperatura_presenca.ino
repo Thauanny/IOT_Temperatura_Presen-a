c1dd7dfa828342bb9821b9da48a62f58d7a3c2df
Gabriel Santana Oliveira de Paula <gabsop@hotmail.com>
2023-12-07 10:32:47 -0300

    Update temperatura_presenca.ino

--- temperatura_presenca.ino
+++ temperatura_presenca.ino
@@ -5,12 +5,24 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <PubSubClient.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <NTPClient.h>
+
+struct Student {
+  String name;
+  String UID;
+  bool state;
+};
+
+struct Student students[2] = {
+  {"Gabriel", "99 135 252 148", 0},
+  {"Lucas", "115 46 136 17", 0}
+};
 
 int num=0;
 String str;
 String s;
-
-
 WiFiClient wifi_client;
 PubSubClient mqtt_client(wifi_client);
 
@@ -23,17 +35,25 @@ const int mqtt_port = 1883;
 int mqtt_timeout = 10000;
 
 const char* mqtt_usernameAdafruitIO = "Gabsop";
-const char* mqtt_keyAdafruitIO = "aio_sAzW76HtuoLbdgIbtJ3i7AciBblX";
+const char* mqtt_keyAdafruitIO = "aio_gnHE33fZI3Lgq86hm0Svn7hM8VkJ";
 
 #define ONBOARD_LED 2
 #define DHTPIN 4
 #define DHTTYPE DHT11
-
-#define ENTRY_PIN 22
-#define EXIT_PIN 23
 #define MIN_PEOPLE_TO_ADJUST 3
 
+// RFID Pins
+#define SS_PIN  5  // ESP32 pin GPIO5 
+#define RST_PIN 27 // ESP32 pin GPIO27
+
+// Telegram
+#define BOTtoken = "6811241521:AAERWvA5qVXYoD-Yx3OqwQ3Txa5hHOV9s0Y";
+
+MFRC522 rfid(SS_PIN, RST_PIN);
 DHT dht(DHTPIN, DHTTYPE);
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 
 int people = 0;
 int targetTemperature = 25;
@@ -45,71 +65,136 @@ void setup() {
   Serial.print(F("People: "));
   Serial.println(people);
 
-  //Arquivo
-  //formatar arquivo ao iniciar
-  formatFile();
-  //se precisar ler descomentar esse trecho
-  //Serial.println("abrir arquivo");
-  //openFS();
-  //String alunos = readFile("/alunos.txt");
-
-  // Fim Arquivo
-
   // Configurando Wifi
   WiFi.mode(WIFI_STA); //"station mode": permite o ESP32 ser um cliente da rede WiFi
   WiFi.begin(wifi_ssid, wifi_password);
   connectWiFi();
   mqtt_client.setServer(mqtt_broker, mqtt_port);
+  
+  // RFID
+  SPI.begin(); // init SPI bus
+  rfid.PCD_Init(); // init MFRC522 
 
+  // Biblioteca de data
+  timeClient.begin();
+  timeClient.setTimeOffset(-10800);
+  timeClient.update();
+
+  // Arquivo
+  // Formatar arquivo ao iniciar
+  // formatFile();
+  openFS();
+  updateVariables("/alunos.txt");
 
   // Configurando Pinos
-  pinMode(ENTRY_PIN, INPUT);
-  pinMode(EXIT_PIN, INPUT);
   pinMode(ONBOARD_LED,OUTPUT);
-
   dht.begin();
 }
 
 void loop() {
   delay(500);
+  timeClient.update();
 
   if (!mqtt_client.connected()) { // Se MQTT não estiver conectado
     connectMQTT();
   }
 
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
+  readCard();
+}
 
-  if (isnan(humidity) || isnan(temperature)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
+void updateVariables(String path) {
+  Serial.println("Read file");
+  File rFile = SPIFFS.open(path, "r");//r+ leitura e escrita
+  if (!rFile) {
+    Serial.println("Erro ao abrir arquivo!");
   }
-
-  if (digitalRead(ENTRY_PIN) == HIGH && mqtt_client.connected()) {
-    people++;
-    Serial.print(F("People: "));
-    Serial.println(people);
-    mqtt_client.publish("Gabsop/feeds/tempwarden-pro-max-v2.people-in-room", String(people).c_str(), true);
-    //substituir pelo codigo do RFID
-    writeFile("ID: " + String(3123123) + "Nome: " + "Thauanny" + "Data: " + "20/06/2321" + "Estado: " + "Entrou", "/alunos.txt");
-
-    Serial.println(F("Someone entered the room"));
-    if (people % MIN_PEOPLE_TO_ADJUST == 0) {
-      adjustTemperatureMessage(true, temperature, humidity); // Increase temperature
+  else {
+    String date = timeClient.getFormattedDate();
+    int indexOfT = date.indexOf("T");
+    date.remove(indexOfT, date.length());
+    while (rFile.position() < rFile.size()) {
+      s = rFile.readStringUntil('\n');
+      for (int i = 0; i < 2; ++i) {
+        if(s.indexOf(students[i].name) > 0 && s.indexOf(date) > 0 && s.indexOf("Entrou") > 0) {
+          students[i].state = 1;
+          people++;
+        }
+        if(s.indexOf(students[i].name) > 0 && s.indexOf(date) > 0 && s.indexOf("Saiu") > 0) {
+          students[i].state = 0;
+          people--;
+        }
+      }
+      s.trim();
+      Serial.println(s);
     }
+    rFile.close(); 
   }
+}
 
-  if (digitalRead(EXIT_PIN) == HIGH && mqtt_client.connected() && people > 0) {
-    people--;
-    Serial.print(F("People: "));
-    Serial.println(people);
-    mqtt_client.publish("Gabsop/feeds/tempwarden-pro-max-v2.people-in-room", String(people).c_str(), true);
-    //substituir pelo codigo do RFID
-    writeFile("ID: " + String(3123123) + "Nome: " + "Thauanny" + "Data: " + "20/06/2321" + "Estado: " + "Saiu" , "/alunos.txt");
-    Serial.println(F("Someone left the room"));
-    if (people % MIN_PEOPLE_TO_ADJUST == 0) {
-      adjustTemperatureMessage(false, temperature, humidity); // Decrease temperature
-    }   
+void readCard() {
+if (rfid.PICC_IsNewCardPresent()) { // new tag is available
+    if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
+
+      String UID;
+      Serial.print("UID:");
+      for (int i = 0; i < rfid.uid.size; i++) {
+        UID.concat(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        UID.concat(rfid.uid.uidByte[i]);
+        UID.trim();
+      }                 
+   
+      String date = timeClient.getFormattedDate();
+      String time = timeClient.getFormattedTime();
+      int indexOfT = date.indexOf("T");
+      date.remove(indexOfT, date.length());
+
+      float humidity = dht.readHumidity();
+      float temperature = dht.readTemperature();
+
+      if (isnan(humidity) || isnan(temperature)) {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+      }
+
+      String studentName;
+      String state = "Entrou";
+      for (int i = 0; i < 2; ++i) {
+        if (students[i].UID == UID) {
+          studentName = students[i].name;
+          if(students[i].state == 0) {
+            state = "Entrou";
+            people++;
+            Serial.print(F("People: "));
+            Serial.println(people);
+            students[i].state = 1;
+          }
+          else {
+            state = "Saiu";
+            people--;
+            Serial.print(F("People: "));
+            Serial.println(people);
+            students[i].state = 0;
+          }
+        }
+      }
+
+      if(studentName == "") {
+        studentName = "Estudante não cadastrado";
+      }
+      
+      writeFile("ID: " + String(UID) + " Nome: " + studentName + " Data: " + date + " Hora: " + time + " Estado: " + state , "/alunos.txt"); 
+      if(mqtt_client.connected()) {
+        mqtt_client.publish("Gabsop/feeds/tempwarden-pro-max-v2.people-in-room", String(people).c_str(), true);
+        if (people % MIN_PEOPLE_TO_ADJUST == 0) {
+          adjustTemperatureMessage(false, temperature, humidity); // Decrease temperature
+        } else {
+          adjustTemperatureMessage(true, temperature, humidity); // Increase temperature
+        }
+      }     
+
+      rfid.PICC_HaltA(); // halt PICC
+      rfid.PCD_StopCrypto1(); // stop encryption on PCD
+    }
   }
 }
 
@@ -170,6 +255,7 @@ void connectMQTT() {
     if (mqtt_client.connect("ESP32Client", mqtt_usernameAdafruitIO, mqtt_keyAdafruitIO)) {
       Serial.println();
       Serial.print("Conectado ao broker MQTT!");
+      mqtt_client.publish("Gabsop/feeds/tempwarden-pro-max-v2.people-in-room", String(people).c_str(), true);
     } else {
       Serial.println();
       Serial.print("Conexão com o broker MQTT falhou!");
